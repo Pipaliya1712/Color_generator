0639e94a01a49e19bbbf1c91981820f02cd12b40
Parth Pipaliya <parthpipaliya1712@gmail.com>
2023-08-20 22:35:20 +0530

    script file definition and fuction initalization

---
+++ script.js
@@ -0,0 +1,13 @@
+// function named Getcolour for generate diff color
+const getColour = () => {
+    
+}
+
+// click event and action on it
+document.getElementById("btn").addEventListener(
+    "click",
+    getColour
+) 
+
+// initial getColour call
+getColour();
