5e239c2593c4c1d45d25fd618de787c1d27d61fa
Parth Pipaliya <parthpipaliya1712@gmail.com>
2023-08-20 22:36:42 +0530

    complite script file

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // function named Getcolour for generate diff color
 const getColour = () => {
-    
+    const randumNumber = Math.floor(Math.random()*16777215);
+    const randumCode = "#" + randumNumber.toString(16);
+    document.body.style.backgroundColor= randumCode
+    document.getElementById("btn").style.backgroundColor = randumCode
+    document.getElementById("text").innerText = randumCode
+    navigator.clipboard.writeText(randumCode)
 }
 
 // click event and action on it
